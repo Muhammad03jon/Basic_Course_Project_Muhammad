7c67e9e1d0dcd5e76cb0fe7da72c95e6d4110bb8
Muhammad03jon <163680576+Muhammad03jon@users.noreply.github.com>
2025-01-04 19:11:37 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,17 @@
 import streamlit as st
+import pandas as pd
 
 st.title('My first website')
 
 st.write('Тут я задеплою модель классификации')
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned")
+
+with st.expander('Data'):
+  st.write("X")
+  X_raw = df.drop('species', axis=1)
+  st.dataframe(X_raw)
+
+st.write("y")
+y_raw = df.species
+st.dataframe(y_raw)
