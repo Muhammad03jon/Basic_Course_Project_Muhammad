f1ac284f199272c72d1c10130c290cd0dcc9dd25
Muhammad03jon <163680576+Muhammad03jon@users.noreply.github.com>
2025-01-04 19:22:48 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,8 @@ with st.expander('Data'):
   y_raw = df.species
 
   st.dataframe(y_raw)
+
+with st.sidebar:
+ st.header('Введите признаки: ')
+ island = st.selectbox('Island', ('Torgersen', 'Dream', 'Biscoe'))
+ bill_lenght_mm = st.slider('Bill lenght (mm)', 32.1, 59.6, 44.5)
